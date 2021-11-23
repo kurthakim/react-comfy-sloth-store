1e0b75bb7150d0b9572fc5e232572d182dd0b26c
Kurt Hakim <kurthakim@gmail.com>
2021-11-23 15:09:55 +0700

    initial commit

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled from 'styled-components';
+import { PageHero } from '../components';
+import aboutImg from '../assets/hero-bcg.jpeg';
+
+const AboutPage = () => {
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="nice desk" />
+        <article>
+          <div className="title">
+            <h2>our story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Necessitatibus numquam beatae unde, rem eius temporibus similique
+            hic quidem minima ipsa mollitia cumque, provident dolores iusto,
+            fugit facilis placeat tenetur nam? Ipsam sit vel perspiciatis nihil
+            nemo, inventore maxime iste at commodi labore laudantium dolorem!
+            Facere debitis deserunt esse rerum corrupti.
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
+
+const Wrapper = styled.section`
+  display: grid;
+  gap: 4rem;
+  img {
+    width: 100%;
+    display: block;
+    border-radius: var(--radius);
+    height: 500px;
+    object-fit: cover;
+  }
+  p {
+    line-height: 2;
+    max-width: 45em;
+    margin: 0 auto;
+    margin-top: 2rem;
+    color: var(--clr-grey-5);
+  }
+  .title {
+    text-align: left;
+  }
+  .underline {
+    margin-left: 0;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`;
+export default AboutPage;
